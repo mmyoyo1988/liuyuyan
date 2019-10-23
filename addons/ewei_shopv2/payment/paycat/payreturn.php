2e17f926e08b394224a18b2fbd7b0e0b7a6f8c4e
mmyoyo1988 <125124332@qq.com>
2019-10-23 18:31:42 +0800

    xx

--- addons/ewei_shopv2/payment/paycat/payreturn.php
+++ addons/ewei_shopv2/payment/paycat/payreturn.php
@@ -1,17 +1,21 @@
 ﻿<?php
-/**
- * ---------------------支付成功，用户会跳转到这里-------------------------------
- * 
- * 此页就是您之前传给http://pay.ebkf.net的return_url页的网址
- * 支付成功，平台会把用户跳转回这里。
- * 
- * --------------------------------------------------------------
- */
 
     $orderid = $_GET["orderid"];
-
-    //此处在您数据库中查询：此笔订单号是否已经异步通知给您付款成功了。如成功了，就给他返回一个支付成功的展示。
     echo "恭喜，支付成功!，订单号：".$orderid;
-    
+    $start = substr($orderid,0,2);
+    if($start == 'RC'){
+        $paytype = 1;
+    }else{
+        $paytype = 0;
+    }
+    if ($paytype == 0) {
+        $url = $_W['siteroot'] . '../../app/index.php?i=' . $uniacid . '&c=entry&m=ewei_shopv2&do=mobile&r=order.pay_alipay.complete&alidata=' . $get;
+    }
+    else if ($paytype == 1) {
+        $url = $_W['siteroot'] . '../../app/index.php?i=' . $uniacid . '&c=entry&m=ewei_shopv2&do=mobile&r=order.pay_alipay.recharge_complete&alidata=' . $get;
+    }
+    //此处在您数据库中查询：此笔订单号是否已经异步通知给您付款成功了。如成功了，就给他返回一个支付成功的展示。
 
+    header('location: ' . $url);
+    exit();
 ?>
